095f35e30aeba8889305ff054459dbf45113dd52
scotchfield <swrittenb@gmail.com>
2015-09-10 21:24:06 -0400

    Basic title, transition between game and map

--- src/js/game.js
+++ src/js/game.js
@@ -1,2 +1,57 @@
-rl.create('game_canvas')
-    .write('Hello, World!', 1, 1);
+var game = (function () {
+	'use strict';
+
+	var canvas, state, render_cb = [],
+		width = 40, height = 40,
+
+	options = {
+		font: '14pt monospace',
+		tileWidth: 16,
+		tileHeight: 16,
+		width: width,
+		height: height,
+	};
+
+	render_cb.removeCb = function(cb) {
+		for (var i = this.length; i >= 0; i -= 1) {
+			if (this[i] === cb) {
+				this.splice(i, 1);
+			}
+		}
+
+		return this;
+	};
+
+	var render = function () {
+		render_cb.forEach(function (cb) {
+			cb();
+		});
+	},
+	setup = function () {},
+
+	renderTitle = function () {
+		rl.write('Hello, World!', 1, 1);
+	},
+	renderMap = function () {
+		rl.clear();
+	},
+
+	keydownTitle = function keydown(e) {
+		rl.setTiles([])
+			.unregisterKeydown(keydown)
+			.registerKeydown(keydownMap);
+		render_cb.removeCb(renderTitle)
+			.push(renderMap);
+		setup();
+		render();
+	},
+	keydownMap = function keydown(e) {};
+
+	rl.create('game_canvas', options)
+		.registerKeydown(keydownTitle);
+
+	state = 'title';
+	render_cb.push(renderTitle);
+	render();
+
+}());
